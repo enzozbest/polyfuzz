c88e125280f7d560f3090c8ad4cfb7c747413dc1
Enzo Zuanazi Bestetti <enzobestetti3@gmail.com>
2026-01-28 16:16:14 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.3.0"
+    jacoco
 }
 
 group = "bestetti.enzo"
@@ -19,4 +20,23 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.named("jacocoTestReport"))
+}
+
+tasks.named<JacocoReport>("jacocoTestReport") {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks.withType<JacocoReport>().configureEach {
+    classDirectories.setFrom(
+        classDirectories.files.map { file ->
+            fileTree(file) {
+                exclude("**/*\$DefaultImpls.class")
+            }
+        }
+    )
 }
