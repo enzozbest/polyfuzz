7bbe25e05819e75f4e3719914bfe248b862acba9
Enzo Zuanazi Bestetti <enzobestetti3@gmail.com>
2026-01-28 22:26:58 +0000

    Introduce the SML Generator using the combinators.

--- smlgen/src/main/kotlin/bestetti/enzo/smlgen/gen/GeneratorConveniences.kt
+++ smlgen/src/main/kotlin/bestetti/enzo/smlgen/gen/GeneratorConveniences.kt
@@ -4,4 +4,12 @@ object GeneratorConveniences {
     val wrapParens: ((String) -> String) = {s ->  "($s)" }
     val wrapBrackets: ((String) -> String) = {s ->  "[$s]" }
     val wrapBraces: ((String) -> String) = {s ->  "{ $s }" }
+
+    fun String.toGenerator() = AtomicGenerators.literal(this)
+    infix fun String.X (other: Generator): Generator = this.toGenerator() X other
+    infix fun Generator.X (other: String): Generator = this X other.toGenerator()
+    infix fun String.X (other: String): Generator = this.toGenerator() X other.toGenerator()
+    infix fun String.F (other: Generator): Generator = this.toGenerator() F other
+    infix fun Generator.F (other: String): Generator = this F other.toGenerator()
+    infix fun String.F (other: String): Generator = this.toGenerator() F other.toGenerator()
 }
